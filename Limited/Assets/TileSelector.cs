3943a43d887aed043ab94eda44555454d2828f79
Florian <florian.rieder@unil.ch>
2019-11-04 23:56:05 +0100

    Sprites + Selector
    - Changed sprites
    - Added some code to the TileSelector so that it stays on scale with the tilemap

--- Limited/Assets/TileSelector.cs
+++ Limited/Assets/TileSelector.cs
@@ -5,12 +5,18 @@ using UnityEngine.Tilemaps;
 
 public class TileSelector : MonoBehaviour
 {
+
+	private float orthoOrg;
+	private float orthoCurr;
+	private Vector3 scaleOrg;
+	private Vector3 posOrg;
+
 	public static TileSelector instance;
 	public Tilemap environmentTilemap;
 
 	public void MoveTo(Vector3Int position)
 	{
-		Vector3 newPos = new Vector3(position.x + 0.5f, position.y + 0.5f, position.z);
+		Vector3 newPos = new Vector3(position.x + 0.5f, position.y + 0.5f, 0);
 		transform.position = newPos;
 	}
 	public void Enabled(bool value)
@@ -18,15 +24,35 @@ public class TileSelector : MonoBehaviour
 		gameObject.SetActive(value);
 	}
 
+	void Start()
+	{
+		// used to make it so that this object doesn't scale with the zoom
+		// stays scaled with tilemap
+		orthoOrg = Camera.main.orthographicSize;
+		orthoCurr = orthoOrg;
+		scaleOrg = transform.localScale;
+		posOrg = Camera.main.WorldToViewportPoint(transform.position);
+	}
+
 	void Update()
 	{
 		Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		Vector3Int tilePos = environmentTilemap.WorldToCell(point);
-        Vector3 newPos = new Vector3(tilePos.x + 0.5f, tilePos.y + 0.5f, tilePos.z);
+		Vector3 newPos = new Vector3(tilePos.x + 0.5f, tilePos.y + 0.5f, tilePos.z);
 
-        if(transform.position != newPos){
-            transform.position = newPos;
-        }
+		if (transform.position != newPos)
+		{
+			transform.position = newPos;
+		}
+
+		// don't scale
+		var osize = Camera.main.orthographicSize;
+		if (orthoCurr != osize)
+		{
+			transform.localScale = scaleOrg * orthoOrg / osize;
+			orthoCurr = osize;
+			transform.position = Camera.main.ViewportToWorldPoint(posOrg);
+		}
 
 	}
 	private void Awake()
